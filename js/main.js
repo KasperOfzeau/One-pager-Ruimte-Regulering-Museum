b41a7651fe339e7c8963b4f7e1074243e33a250b
Kasper Beljaars <kasperbeljaars@hotmail.com>
2020-10-26 14:59:24 +0100

    Vdieo toegevoegd

--- js/main.js
+++ js/main.js
@@ -31,24 +31,24 @@ $(document).ready(function() {
 });
 
 $(document).ready(function() {
-// Modal opvragen
-var modal = document.getElementById("Modal");
+  // Modal opvragen
+  var modal = document.getElementById("Modal");
 
-// Vul content van modal
-var img = document.getElementById("demo-img");
-var modalImg = document.getElementById("img01");
-var captionText = document.getElementById("caption");
-img.onclick = function(){
-  modal.style.display = "block";
-  modalImg.src = this.src;
-  captionText.innerHTML = this.alt;
-}
+  // Vul content van modal
+  var img = document.getElementById("demo-img");
+  var modalImg = document.getElementById("img01");
+  var captionText = document.getElementById("caption");
+  img.onclick = function() {
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+  }
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+  // Sluit knop modal
+  var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
+  // Modal afsluiten
+  span.onclick = function() {
+    modal.style.display = "none";
+  }
 });
